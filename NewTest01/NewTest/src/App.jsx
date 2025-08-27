52164e18cab4eb2d82f32245ea8006b7d995403c
Rushan Herath <rushan@libt.co.uk>
2025-08-27 13:31:54 +0100

    new

--- NewTest01/NewTest/src/App.jsx
+++ NewTest01/NewTest/src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react'
+import Dashboard from './components/Dashboard'
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <>
-      <navBar />
+      <Navbar/>
       <Dashboard />
     </>
   )
